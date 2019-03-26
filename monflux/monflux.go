9431a84ca3e9738408fa6e6c581d4327147fde7f
Jeff Wendling <leterip@gmail.com>
2019-03-26 16:47:23 -0600

    put all data behind mon measurement

--- monflux/monflux.go
+++ monflux/monflux.go
@@ -25,28 +25,30 @@ func (e *errWriter) Write(p []byte) (int, error) {
 
 func Write(w io.Writer) error {
 	ew := &errWriter{w: w}
-	outputf := func(m, f string, val float64) { fmt.Fprintf(ew, "%s %s=%v\n", m, f, val) }
-	outputi := func(m, f string, val int64) { fmt.Fprintf(ew, "%s %s=%di\n", m, f, val) }
-	outputq := func(m string, his *mon.Histogram, q float64) {
-		fmt.Fprintf(ew, "%s,percentile=%v value=%v\n", m, q, float64(his.Quantile(q))/1e9)
-	}
 	mon.Times(func(name string, state *mon.State) bool {
 		current, total := state.Current(), state.Total()
-		outputi(name, "current", current)
-		outputi(name, "total", total)
+		fmt.Fprintf(ew, "mon,name=%q current=%di\n", name, current)
+		fmt.Fprintf(ew, "mon,name=%q total=%di\n", name, total)
 		for iter := state.Errors().Iterator(); iter.Next(); {
 			err, count := iter.Key(), atomic.LoadInt64((*int64)(iter.Value()))
-			outputi(fmt.Sprintf("%s,error=%q", name, err), "count", count)
+			fmt.Fprintf(ew, "mon,name=%q,error=%q count=%di\n", name, err, count)
 		}
+
 		if _, average := state.Average(); !math.IsNaN(average) {
-			outputf(name, "average", average/1e9)
+			fmt.Fprintf(ew, "mon,name=%q average=%v\n", name, average/1e9)
+
 			his := state.Histogram()
-			outputq(name, his, 0)
-			outputq(name, his, 0.5)
-			for i, p := int64(10), float64(0.1); i/2 < total; i, p = i*10, p/10 {
-				outputq(name, his, 1-p)
+			outputq := func(q float64) {
+				value := float64(his.Quantile(q)) / 1e9
+				fmt.Fprintf(ew, "mon,name=%q,percentile=%v value=%v\n", name, q, value)
 			}
-			outputq(name, his, 1)
+
+			outputq(0)
+			outputq(0.5)
+			for i, p := int64(10), float64(0.1); i/2 < total; i, p = i*10, p/10 {
+				outputq(1 - p)
+			}
+			outputq(1)
 		}
 
 		return ew.err == nil
